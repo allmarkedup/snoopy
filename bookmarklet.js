e619af42f30aa389040d7610c88fe718dcca6454
Mark Perkins <mark@medium.io>
2010-09-06 18:10:57 +0100

    Move bookmarklet code to top and add not about usage.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,6 @@
+// bookmarklet source: paste this into a bookmark URL field to add as a bookmarklet
+javascript:(function()%7Bvar%20d%3Ddocument%2Cs%2Ce%3Bvar%20el%3Dd.getElementById('snoopy')%3Bif(el)%7Bel.className%3Del.className.replace('closed'%2C'')%3Breturn%7Ds%3Dd.createElement('link')%3Bs.setAttribute('href'%2C'http%3A%2F%2Fsnoopy-js.s3.amazonaws.com%2Fsnoopy.css')%3Bs.setAttribute('rel'%2C'stylesheet')%3Bs.setAttribute('type'%2C'text%2Fcss')%3Bd.getElementsByTagName('head')%5B0%5D.appendChild(s)%3Be%3Dd.createElement('script')%3Be.setAttribute('src'%2C'http%3A%2F%2Fsnoopy-js.s3.amazonaws.com%2Fsnoopy.js')%3Bd.getElementsByTagName('body')%5B0%5D.appendChild(e)%7D)()%3B
+
 // Uncompressed bookmarklet code:
 (function(){
     
@@ -24,6 +27,3 @@
     d.getElementsByTagName('body')[0].appendChild(e);
     
 })();
-
-// bookmarklet source:
-javascript:(function()%7Bvar%20d%3Ddocument%2Cs%2Ce%3Bvar%20el%3Dd.getElementById('snoopy')%3Bif(el)%7Bel.className%3Del.className.replace('closed'%2C'')%3Breturn%7Ds%3Dd.createElement('link')%3Bs.setAttribute('href'%2C'http%3A%2F%2Fsnoopy-js.s3.amazonaws.com%2Fsnoopy.css')%3Bs.setAttribute('rel'%2C'stylesheet')%3Bs.setAttribute('type'%2C'text%2Fcss')%3Bd.getElementsByTagName('head')%5B0%5D.appendChild(s)%3Be%3Dd.createElement('script')%3Be.setAttribute('src'%2C'http%3A%2F%2Fsnoopy-js.s3.amazonaws.com%2Fsnoopy.js')%3Bd.getElementsByTagName('body')%5B0%5D.appendChild(e)%7D)()%3B
