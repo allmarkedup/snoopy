94e8b9c1ab4a6b63bf4b955e63fcce991fbf67d8
Mark Perkins <mark@medium.io>
2010-12-30 21:21:30 +0000

    Update Tim to latest ('lite') version.

--- src/lib/tim.js
+++ src/lib/tim.js
@@ -1,27 +1,54 @@
-    // tim - A tiny JavaScript micro-templating script.
-    // http://gist.github.com/521352
-    var tim = (function(){
-        var starts  = "{{",
-            ends    = "}}",
-            path    = "[a-z0-9_][\\.a-z0-9_]*", // e.g. config.person.name
-            pattern = new RegExp(starts + "("+ path +")" + ends, "gim"),
-            length  = "length",
-            undef;
+    /*!
+    * Tim (lite)
+    *   github.com/premasagar/tim
+    *
+    *//*
+        A tiny, secure JavaScript micro-templating script.
+    *//*
 
+        by Premasagar Rose
+            dharmafly.com
+
+        license
+            opensource.org/licenses/mit-license.php
+
+        **
+
+        creates global object
+            tim
+
+        **
+        
+        v0.3.0
+        
+    */
+
+    var tim = (function(){
+        "use strict";
+
+        var start  = "{{",
+            end    = "}}",
+            path    = "[a-z0-9_][\\.a-z0-9_]*", // e.g. config.person.name
+            pattern = new RegExp(start + "\\s*("+ path +")\\s*" + end, "gi"),
+            undef;
+    
         return function(template, data){
-            return template.replace(pattern, function(tag){
-                var ref = tag.slice(starts[length], 0 - ends[length]),
-                    path = ref.split("."),
-                    len = path[length],
+            // Merge data into the template string
+            return template.replace(pattern, function(tag, token){
+                var path = token.split("."),
+                    len = path.length,
                     lookup = data,
                     i = 0;
 
                 for (; i < len; i++){
-                    if (lookup === undef){
-                        break;
-                    }
                     lookup = lookup[path[i]];
-
+                
+                    // Property not found
+                    if (lookup === undef){
+                        throw "tim: '" + path[i] + "' not found in " + tag;
+                    }
+                
+                    // Return the required value
                     if (i === len - 1){
                         return lookup;
                     }
